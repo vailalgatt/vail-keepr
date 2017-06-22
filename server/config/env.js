3bbcbf787cfef8632fa31c9af90f915334484034
MarkOhnsman <MarkOhnsman@users.noreply.github.com>
2017-06-22 16:51:04 -0600

    Update env.js

--- server/config/env.js
+++ server/config/env.js
@@ -3,10 +3,10 @@ let env = {
 	NODE_ENV: 'development',
 	PORT: 3000,
 	DBPROTOCOL: 'mongodb',
-	DBUSERNAME: 'student',
-	DBPASSWORD: 'CodeWorksStudent',
-	DBHOST: 'ds056789.mlab.com:56789',
-	DBNAME: 'codeworks',
+	DBUSERNAME: //your username here,
+	DBPASSWORD: //your password here',
+	DBHOST: //DB Host name,
+	DBNAME: //DB name,
 	SERVERNAME: 'dev-server'
 }
 
