640a51cd03c8ee8ee2e5e68ff93d4e0e2061edd8
Vail Algatt <vail.algatt@gmail.com>
2017-06-25 12:08:33 -0600

    fixing some broken stuff

--- server/models/keeps.js
+++ server/models/keeps.js
@@ -4,9 +4,15 @@ let mongoose = require('mongoose')
 let ObjectId = mongoose.Schema.ObjectId
 
 var schema = new mongoose.Schema({
-	name: { type: String, required: true },
+	title: { type: String, required: true },
 	description: { type: String },
+	imgUrl: { type: String, required: true },
 	created: { type: Number, default: Date.now() },
+	creatorId: { type: ObjectId, ref: modesl.user.name, required: true },
+	author: { type: ObjectId, ref: models.user.name, required: true },
+	keepCount: { type: Number, defualt: 0 },
+	shareCount: { type: Number, defualt: 0 },
+	viewCount: { type: Number, defualt: 0 },
 	// Relations
 	vaultId: { type: ObjectId, ref: models.vault.name, required: true },
 
--- server/models/vault.js
+++ server/models/vault.js
@@ -5,9 +5,10 @@ let ObjectId = mongoose.Schema.ObjectId
 var schema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String },
+    imgUrl: { type: String },
     created: { type: Number, default: Date.now() },
-    createrId: { type: Objectid, ref: models.user.name, required: true},
-    keeps: [{type: ObjectId, ref: models.keeps.name}]
+    createrId: { type: Objectid, ref: models.user.name, required: true },
+    keeps: [{ type: ObjectId, ref: models.keeps.name, required: true}]
 });
 
 module.exports = mongoose.model(models.vault.name, schema);
