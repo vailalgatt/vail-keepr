ca148813195413177c59cb19012607b85c02616f
Vail Algatt <vail.algatt@gmail.com>
2017-06-26 14:10:23 -0600

    publishing keeps without details

--- www/src/components/Keeps.vue
+++ www/src/components/Keeps.vue
@@ -1,5 +1,27 @@
 <template>
     <div class="keeps">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <h1 class="keepNav">Keeps</h1>
+                </div>
+                    <form class="navbar-form" role="search">
+                        <div class="input-group">
+                            <div class="col-xs-12">
+                            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                            </div>
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="dungeon" style="color:black; cursor: pointer" @click="logout(this.user)">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <div class="well">
             <ol>
                 <li v-for="keep in keeps">
@@ -23,18 +45,18 @@
             this.$store.dispatch('getKeeps')
         },
         computed: {
-            keeps(){
+            keeps() {
                 return this.$store.state.keeps
             }
         },
         methods: {
-            createKeep(){
+            createKeep() {
                 this.$store.dispatch('createKeep', {
                     name: 'Brand new KEEP!',
                     description: 'HIYA'
                 })
             },
-            addKeep(){
+            addKeep() {
                 //drag and drop? 
             }
         },
@@ -45,5 +67,13 @@
 
 
 <style>
-
+    input {
+        color: black;
+        font-family: 'Open Sans', sans-serif;
+        margin-bottom: 10px;
+        font-size: 5px;
+    }
+button{
+    font-weight: bold;
+}
 </style>
--- www/src/components/Vaults.vue
+++ www/src/components/Vaults.vue
@@ -1,24 +1,79 @@
 <template>
     <div class="vaults">
-
-
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <h1 class="keepNav">Keeps</h1>
+                </div>
+                    <form class="navbar-form" role="search">
+                        <div class="input-group">
+                            <div class="col-xs-12">
+                            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                            </div>
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="dungeon" style="color:black; cursor: pointer" @click="logout(this.user)">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="well">
+            <ol>
+                <li v-for="keep in keeps">
+                    <router-link :to=" '/keeps/'+ keep._id">
+                        <div class="well">
+                            {{keep.name}}
+                            <button class="fa fa-trash" aria-hidden="true" @click="addKeep()"></button>
+                        </div>
+                    </router-link>
+                </li>
+            </ol>
+        </div>
     </div>
 </template>
 
 
 <script>
-export default {
-    name: 'vaults',
-    data(){
+    export default {
+        name: 'vaults',
+        data() {
+            this.$store.dispatch('getKeeps')
+        },
+        computed: {
+            keeps() {
+                return this.$store.state.keeps
+            }
+        },
+        methods: {
+            createKeep() {
+                this.$store.dispatch('createKeep', {
+                    name: 'Brand new KEEP!',
+                    description: 'HIYA'
+                })
+            },
+            addKeep() {
+                //drag and drop? 
+            }
+        },
+        components: {}
+    }
 
-    },
-    computed:{},
-    methods:{},
-    components:{}
-}
 </script>
 
 
 <style>
-
+    input {
+        color: black;
+        font-family: 'Open Sans', sans-serif;
+        margin-bottom: 10px;
+        font-size: 5px;
+    }
+button{
+    font-weight: bold;
+}
 </style>
